b9ec6eb58b993f6c7db4a2ed51f05ade6b3ba0fb
Nome Funcionario <violisantana@gmail.com>
2020-08-19 16:46:58 -0300

    PaginaInformativa

---
+++ java.js
@@ -0,0 +1,15 @@
+var doc = document.querySelector('header')
+
+function mudaCorFundo() {
+    if (doc.className == 'escuro') {
+        doc.className = 'claro'
+    }
+    else {
+        doc.className = 'escuro'
+    }
+
+}
+
+
+
+
